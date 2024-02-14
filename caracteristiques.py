50eab5c3a53824b3b718fd1a74e1a4acee244446
Julie C <julie.ciesla@ens.uvsq.fr>
2024-02-14 06:16:45 +0100

    modification de la doc + harmonisation variables

--- caracteristiques.py
+++ caracteristiques.py
@@ -1,67 +1,94 @@
 import re
 
-filename = "graph2.txt"
+filename = "FichierTests/graph2.txt"
 
 
 def ReadGraph(filename):
-    """ fonction qui permet de mettre un graphe sous forme de fichier texte dans une liste où chaque \
-         élément de la liste correspond aux arêtes du noeuds dont c'est l'indice """
+    """ 
+    Fonction qui permet de mettre un graphe issu d'un fichier texte généré via le programme Plantri
+    dans une liste où chaque élément de la liste correspond aux arêtes du noeuds dont c'est l'indice. 
+
+    Entrée: Fichier texte (.txt) incluant la représentation Plantri d'un graphe
+    Sortie: Liste où chaque élément de la liste correspond aux arêtes du noeud dont c'est l'indice 
+
+    Exemple:
+        Entrée: 1[2 3 4 5] 2[1 5 4 3] 3[1 2 4] 4[1 3 2 5] 5[1 4 2]
+        Sortie: [[2, 3, 4, 5], [1, 5, 4, 3], [1, 2, 4], [1, 3, 2, 5], [1, 4, 2]]
+    """
 
     with open(filename, "r") as filin:
-        ligne = filin.readline() # on a une ligne par fichier
-        liste = re.findall(r"\[[\d|\s]+\]", ligne) # avec une expression régulière on met dans une liste tous les éléments entre crochets
+        line = filin.readline() # Lecture de la 1ère ligne du fichier
+        # Avec une expression régulière on met dans une liste tous les éléments qui sont entre crochets []
+        liste = re.findall(r"\[[\d|\s]+\]", line)
         graph = list()
-        for noeud in liste :
-            noeud = noeud.replace("[", "")
-            noeud = noeud.replace("]", "")
-            noeud = noeud.split(" ")
-            graph.append(noeud) # indice de la liste correspond au noeud 
 
-        graph = [[int(i) for i in sous_graph] for sous_graph in graph]
+        for vertex in liste :
+            vertex = vertex.replace("[", "")
+            vertex = vertex.replace("]", "")
+            vertex = vertex.split(" ")
+            graph.append(vertex) # indice de la liste correspond au noeud 
+
+        # Tous les éléments de la liste deviennent de type int
+        graph = [[int(i) for i in sub_graph] for sub_graph in graph]
         
         return graph
 
-graphe = ReadGraph(filename)
-print(graphe)
+graph = ReadGraph(filename)  # Remarque: pas une var globale mais est quand meme connu par les fonctions ci dessous ???
+print(graph)
+ 
 
-# comment faire pour l'orientation ? Dans notre fichier, elle est mauvaise 
+def VertexDegrees(vertex):
+    """ 
+    Fonction qui renvoie le degré d'un sommet.
+    Entrée: Sommet (int)
+    Sortie: Degré du sommet (int)
+    """
+    return len(graph[vertex-1])
 
-def Degreesnoeuds(noeuds):
-    """ fonction qui renvoie le nombre d'arêtes relié à un noeud """
-    return len(graphe[noeuds-1])
 
+def RightFaceCardinality(edge):
+    """ 
+    Fonction qui donne le nombre arêtes sur la face immédiatement à droite d'un arc. 
+    (i.e. donne la taille de la face) 
 
-def NombreAretesFaceDroite(arete):
-    """ fonction qui donne le nombre arêtes sur la face droite d'une arête  : arête (queue, tête) """
+    Entrée: Arc sous forme de tuple (queue, tete)
+    Sortie: Taille de la face immédiatement à droite de "arete" (int)
+    """
     
-    tail = arete[0]
-    head = arete[1]
+    tail = edge[0]
+    head = edge[1]
     count = 1
 
-    while head != arete[0]:
-        indice = graphe[head-1].index(tail) # on met dans une variable la place de l'arête (head, tail)
+    while head != edge[0]:
+        indice = graph[head-1].index(tail) # on met dans une variable la place de l'arête (head, tail)
         tail = head 
-        head = graphe[head-1][(indice+1)%len(graphe[head-1])] # la nouvelle valeur du sommet qui est la tête est à droite (+)
+        head = graph[head-1][(indice+1)%len(graph[head-1])] # la nouvelle valeur du sommet qui est la tête est à droite (+)
         count += 1
     
     return count
 
-print(NombreAretesFaceDroite((1,2)))
+print(RightFaceCardinality((1,2)))
 
 
-def NombreAretesFaceGauche(arete):
-    """ fonction qui donne le nombre arêtes sur la face gauche d'une arête """
+def LeftFaceCardinality(edge):
+    """ 
+    Fonction qui donne le nombre arêtes sur la face immédiatement à gauche d'un arc. 
+    (i.e. donne la taille de la face) 
+
+    Entrée: Arc sous forme de tuple (queue, tete)
+    Sortie: Taille de la face immédiatement à gauche de "arete" (int)
+    """
     
-    tail = arete[0]
-    head = arete[1]
+    tail = edge[0]
+    head = edge[1]
     count = 1
 
-    while head != arete[0]:
-        indice = graphe[head-1].index(tail) # on met dans une variable la place de l'arête (head, tail)
+    while head != edge[0]:
+        indice = graph[head-1].index(tail) # on met dans une variable la place de l'arête (head, tail)
         tail = head 
-        head = graphe[head-1][(indice-1)%len(graphe[head-1])] # la nouvelle valeur du sommet qui est la tête est à gauche (-)
+        head = graph[head-1][(indice-1)%len(graph[head-1])] # la nouvelle valeur du sommet qui est la tête est à gauche (-)
         count += 1
     
     return count
 
-print(NombreAretesFaceDroite((1,2)))
+print(LeftFaceCardinality((1,2)))
